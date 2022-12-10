d7ae1acaa1f166886d57d4ff17466da837bf218a
denisplay <workingdenniss@gmail.com>
2022-12-10 16:59:44 +0200

    remade extract_number function
    now extract_number_from_string using normal syntax without for loop

--- src/extract.rs
+++ src/extract.rs
@@ -1,37 +1,26 @@
 use regex::Regex;
 
-
-//noraml regex
-/*
-let re = Regex::new(r"\d+").unwrap();
-    
-let answer = re.find_iter(file_name);
-for i in answer{
-    println!("{}", i.as_str());
-}
-*/
-
 pub fn extract_number_from_string(file_name: &str) -> i32{
-    let mut numbers_in_array: Vec<&str> = Vec::new();
-    for cap in Regex::new(r"\d+").unwrap().find_iter(file_name) {
-        numbers_in_array.push(cap.as_str());
-    }
+    //create a regex instance
+    let re = Regex::new(r"\d+").unwrap();
+    //search with regex only numbers inside a &str
+    let mut numbers_in_array = re.find_iter(file_name).collect::<Vec<_>>();
 
     if !(numbers_in_array.len() > 1){
-        return numbers_in_array[0].parse().unwrap();
+        return numbers_in_array[0].as_str().parse().unwrap();
     }
-
-    numbers_in_array = Vec::new();
+    //search using the last '-'
     let dash_separator = file_name.split('-');
     let dash_vec: Vec<&str> = dash_separator.collect();
-    for cap in Regex::new(r"\d+").unwrap().find_iter(dash_vec[dash_vec.len() - 1]) {
-        numbers_in_array.push(cap.as_str());
-    }
-
+    numbers_in_array = re.find_iter(dash_vec[dash_vec.len() - 1]).collect::<Vec<_>>();
+    
     if numbers_in_array.len() > 1{
-        return numbers_in_array[1].parse().unwrap();
+        return numbers_in_array[1].as_str().parse().unwrap();
     }
-    else{
-        return numbers_in_array[0].parse().unwrap();
+    else if numbers_in_array.len() > 0 {
+        return numbers_in_array[0].as_str().parse().unwrap();
+    }{
+        return -1;//did not find
     }
 }
+
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 mod extract;
 
 fn main() {
-    let file_name = "Gintama - S01E03.mkv";
+    let file_name = "86 - Eighty Six - S01E02.mkv";
     println!("Hello1");
     let a = extract::extract_number_from_string(file_name);
     println!("Hello2");
