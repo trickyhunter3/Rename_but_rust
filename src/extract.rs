0877a90d11e2bddfb8fcb10b1de35f4485fbb706
denisplay <workingdenniss@gmail.com>
2022-12-11 18:18:49 +0200

    now compiles

--- src/extract.rs
+++ src/extract.rs
@@ -1,4 +1,3 @@
-use std::path::Path;
 use walkdir::{DirEntry, WalkDir};
 
 use regex::Regex;
@@ -9,9 +8,12 @@ fn check_files_extract_number_from_string(file_name: &str) -> i32{
     //search with regex only numbers inside a &str
     let mut numbers_in_array = re.find_iter(file_name).collect::<Vec<_>>();
 
-    if !(numbers_in_array.len() > 1){
-        return numbers_in_array[0].as_str().parse().unwrap();
-    }
+    if numbers_in_array.len() == 1{
+        return numbers_in_array[0].as_str().parse().unwrap();}
+
+    else if numbers_in_array.len() == 0 {
+        return -1;}//did not find
+
     //search using the last '-'
     let dash_separator = file_name.split('-');
     let dash_vec: Vec<&str> = dash_separator.collect();
@@ -33,7 +35,8 @@ fn extract_season_number(directory_name: &str) -> i32{
 
     let numbers_in_array = re.find_iter(directory_name).collect::<Vec<_>>();
     if numbers_in_array.len() > 0{
-        return numbers_in_array[0].as_str().parse().unwrap();}
+        return numbers_in_array[0].as_str().parse().unwrap();
+    }
 
     return -1;
 }
@@ -44,6 +47,24 @@ fn get_file_extention(file_name: &str) -> &str{
     return dot_vec[dot_vec.len() - 1];
 }
 
+fn get_series_name_and_season(file_path: &str, file_depth: usize) -> Vec<&str>{
+    if file_depth != 3{
+        return ["-1", "-1"].to_vec();//no need to find series
+    }
+
+    let slash_seperator = file_path.split('\\');
+    let slash_vec: Vec<&str> = slash_seperator.collect();
+    return [slash_vec[slash_vec.len() - 3], slash_vec[slash_vec.len() - 2]].to_vec();
+}
+
+fn filter_extention(file_extention: &str) -> bool{
+    if file_extention == "ini" || file_extention == "nfo" || file_extention == "ico"{
+        return true;
+    }
+
+    return false;
+}
+
 fn is_hidden(entry: &DirEntry) -> bool {
     entry.file_name()
          .to_str()
@@ -52,35 +73,54 @@ fn is_hidden(entry: &DirEntry) -> bool {
 }
 
 pub fn iter_over_all_files(root_path: &str){
-    let mut current_directory_name: &str = "";
-    let mut current_file_name: &str = "";
-    let mut current_entry;
-
     let walker = WalkDir::new(root_path).into_iter();
-    for entry in walker.filter_entry(|e| !is_hidden(e)) 
-    {
-        current_entry = entry.unwrap();
-        
-        if current_entry.file_type().is_dir(){
-            current_directory_name = current_entry.file_name().to_str().unwrap();
-            println!("Entering Directory: {}", current_directory_name);
-            check_files_extract_number_from_string(current_directory_name);
-        }
-        else{
-            current_file_name = current_entry.file_name().to_str().unwrap();
-            let current_episode_number = check_files_extract_number_from_string(current_file_name);
-            let current_season_number = extract_season_number(current_directory_name);
-            let current_file_extention = get_file_extention(current_file_name);
-            is_file_name_valid(current_file_name, current_directory_name, current_season_number, current_episode_number, current_file_extention);
+    if WalkDir::new(root_path).into_iter().count() > 1 {
+        for entry in walker.filter_entry(|e| !is_hidden(e)){
+            let current_entry = entry.unwrap();
+            
+            let _a = current_entry.file_type().is_dir();
+            if current_entry.file_type().is_dir(){
+                println!("Entering Directory: {}", current_entry.file_name().to_str().unwrap());
+            }
+            else {
+                let current_series_name_and_season: Vec<&str>  = get_series_name_and_season(current_entry.path().to_str().unwrap(), current_entry.depth());
+                let current_file_name = current_entry.file_name().to_str().unwrap();
+                let current_episode_number = check_files_extract_number_from_string(current_file_name);
+                let current_season_number = extract_season_number(current_series_name_and_season[1]);
+                let current_file_extention = get_file_extention(current_file_name);
+                //println!("{}", current_entry.path().display());
+                if !filter_extention(current_file_extention){
+                    println!("file_name: {0}, episode_number: {1}, season_number: {2}, file_extention: {3}", current_file_name, current_episode_number, current_season_number, current_file_extention);
+                    is_file_name_valid(current_file_name, check_files_extract_number_from_string(current_file_name), current_series_name_and_season[0], current_season_number, current_episode_number, current_file_extention);
+                }
+            }
         }
     }
 }
 
-
-fn is_file_name_valid(file_name: &str, directory_name: &str, season_number: i32, episode_number: i32, file_extention: &str) -> bool{
+fn is_file_name_valid(file_name: &str, file_name_number: i32, series_name: &str, season_number: i32, episode_number: i32, file_extention: &str) -> bool{
     if episode_number == -1{
-        return false;}
-    //TODO check how much zero helpers needed
+        return false;
+    }
+    let mut subtitle_helper = "";
+    //TODO helper for episodes
+    //let mut episode_helper: String = String::new();//E01 or E0001
+    //let mut season_helper: String = String::new();//S01 or S0001
+    if file_extention == "ass" {
+        subtitle_helper = ".eng";
+    }
 
     return true;
 }
+
+/* 
+fn helper_create(number: i32) -> String{
+    let mut helper: String = String::new();
+    let mut local_number = number;
+    while local_number / 10 != 0{
+        helper.push_str("0");
+        local_number /= 10;
+    }
+    return helper;
+}
+*/
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod extract;
 fn main() {
     let mut _current_entry: DirEntry;
     let _file_name = "86 - Eighty Six - S01E02.mkv";
-    let b = "C:\\Code\\hello\\";
+    let b = "E:\\a_Projects\\AN\\";
     extract::iter_over_all_files(b);
 }
 
