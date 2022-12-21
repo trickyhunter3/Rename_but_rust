db781a0f68b3ba2d062a30cd3400a3e67c79ab9c
denisplay <workingdenniss@gmail.com>
2022-12-21 13:43:30 +0200

    now multiline works and outputs the paths

--- src/window.rs
+++ src/window.rs
@@ -1,6 +1,7 @@
 use egui::FontFamily::Proportional;
 use egui::FontId;
 use egui::TextStyle::*;
+use std::collections::HashMap;
 use std::fs;//read json
 use serde_json::Value;
 
@@ -47,6 +48,29 @@ fn get_json(folder: String) -> String{
     return value[folder].as_str().unwrap().to_string();//to remove ""
 }
 
+fn get_file_path_no_name(full_name: String) -> String{
+    let mut final_string: String = "".to_string();
+    let slash_seperator = full_name.split('\\');
+    let slash_vec: Vec<&str> = slash_seperator.collect();
+
+    for i in 0..(slash_vec.len() - 1){
+        final_string = final_string +  &slash_vec[i].to_string() + &"\\".to_string();
+    }
+    return final_string;
+}
+
+fn extract_the_directories(wrong_names:Vec<Vec<String>>){
+    let mut directories: HashMap<String, i32> = HashMap::new();
+    for i in wrong_names{
+        for j in i{
+            let path = get_file_path_no_name(j);
+            directories.insert(path, 1);
+        }
+    }
+    for (i,_j) in directories{
+        println!("{}", i);
+    }
+}
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
@@ -67,13 +91,17 @@ impl eframe::App for MyApp {
             ui.heading("Renamer");
             ui.horizontal(|ui| {
                 let name_label = ui.label("Path: ");
-                ui.text_edit_singleline(&mut self.user_path)
+                ui.text_edit_multiline(&mut self.user_path)
                     .labelled_by(name_label.id);
             });
             if ui.button("Rename").clicked(){ 
-                println!("-----------------");
-                extract::iter_rename_files(&self.user_path, self.is_number_first, self.is_number_second, self.is_number_last);
-                println!("-----------------");
+                println!("---------------------------------------------------");
+                let slash_seperator = self.user_path.split('\n');
+                let slash_vec: Vec<&str> = slash_seperator.collect();
+                for i in slash_vec{
+                    extract::iter_rename_files(i, self.is_number_first, self.is_number_second, self.is_number_last);
+                    println!("---------------------------------------------------");
+                }
             }
             ui.add(egui::Checkbox::new(&mut self.is_number_first, "is number first?"));
             ui.add(egui::Checkbox::new(&mut self.is_number_second, "is number second?"));
@@ -81,14 +109,14 @@ impl eframe::App for MyApp {
             if ui.button("Check Files").clicked(){ 
                 let mut wrong_names:Vec<Vec<String>> = Vec::new();
                 let mut was_there_error = false;
-                println!("-----------------");
+                println!("---------------------------------------------------");
                 println!("Anime: {}", &self.json_path_anime);
                 println!("Anime not : {}", &self.json_path_anime_not);
                 wrong_names.push(extract::iter_over_all_files_check_files(&self.json_path_anime));
                 wrong_names.push(extract::iter_over_all_files_check_files(&self.json_path_anime_not));
-                println!("-----------------");
+                println!("---------------------------------------------------");
                 println!("Errors:");
-                for i in wrong_names{
+                for i in &wrong_names{
                     for j in i{
                         println!("{}", j);
                         was_there_error = true;
@@ -96,6 +124,11 @@ impl eframe::App for MyApp {
                 }
                 if !was_there_error{
                     println!("everything is correct!");
+                }
+                else{
+                    println!("---------------------------------------------------");
+                    println!("Directories:");
+                    extract_the_directories(wrong_names);
                 }
             }
 
